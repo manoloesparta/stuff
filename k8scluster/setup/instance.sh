b551d174b8933f5bf11cb1a381faf4027fc3226f
Manuel Esparza <manoloesparta@gmail.com>
2023-02-02 22:51:02 -0600

    master worker

--- k8scluster/setup/instance.sh
+++ k8scluster/setup/instance.sh
@@ -9,9 +9,13 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo usermod -aG docker $USER
 
-# Install Kubernetes
+# Docker setup
+sudo usermod -aG docker $USER
+sudo rm -rf /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+# Install Kubernetes tools
 sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
